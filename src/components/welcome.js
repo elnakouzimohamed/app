5f122955780d024cc230859bd5f5b5b6b5443816
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-13 15:55:30 +0200

    u

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,7 +3,7 @@
 const Welcome = () => {
     return (
         <section className="welcome">
-            <div className="styles.background">
+            <div className="background">
                 <div className="backgroundText">
                     <h1>Welcome to Our Website</h1>
                     <p>Discover our amazing services and products.</p>
