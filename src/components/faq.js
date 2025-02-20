b8b495ad9bfff07cd72ed6c93bcf248add4ee676
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-20 16:19:20 +0200

    i

--- src/components/faq.js
+++ src/components/faq.js
@@ -34,7 +34,7 @@ export default function Faq() {
             <h2 className="text-center text-[2rem] font-[bold] mb-5">Frequently Asked Questions</h2>
             <div className="mx-auto max-w-[1400px]">
                 {faqItems.map((item, index) => (
-                    <div key={index} className="border overflow-hidden shadow-[0_4px_8px_rgba(0,0,0,0.1)]  mx-auto my-0 rounded-[5px] border-solid border-[#ddd] md:w-[500px] sm:w-[250px] w-[200px]">
+                    <div key={index} className="border overflow-hidden shadow-[0_4px_8px_rgba(0,0,0,0.1)]  mx-auto my-0 rounded-[5px] border-solid border-[#ddd] md:w-[500px] sm:w-[250px] w-[250px]">
                         <button className="w-full text-left text-base cursor-pointer font-[bold] flex justify-between items-center p-[15px] border-[none] hover:bg-[#f1f1f1];" style={{ background: "white"}} onClick={() => toggleAnswer(index)}>
                             {item.question}
                             <span className="icon">{openIndexes.includes(index) ? "−" : "+"}</span>
