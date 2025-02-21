d1f861e96c8a7086f13ec717a64ba7a5956fac4a
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-21 17:05:23 +0200

    i

--- src/components/services.js
+++ src/components/services.js
@@ -15,14 +15,15 @@ const Services = () => {
         const filteredServices = selectedService === "All"
     ? services
     : services.filter(service => service.title === selectedService);
-  
+    
+    const styl = filteredServices.length === 1 ? "md:px-[500px] sm:px-[150px] px-[120px]" : "md:px-[110px] sm:px-[80px] px-[50px]";
    
 
     return (
         <section className=" gap-5 flex-wrap pt-2 pb-12">
             <h1 className="text-center font-bold  pt-[70px] pb-5 text-[32px]" >Check Out Our Services</h1>
             <div className="flex items-center justify-center text-center ">
-            <label className="font-bold sm:text-[24px] text-[20px]" >Filter by Service: </label>
+            <label className="font-bold sm:text-[24px] text-[20px] my-2" >Filter by Service: </label>
       <select
         
         value={selectedService}
@@ -36,8 +37,11 @@ const Services = () => {
        
       </select>
       </div>
-             <div className=" flex items-center justify-center mx-auto my-auto md:flex-row flex-col md:gap-8 gap-3 py-[20px] md:px-[110px] sm:px-[80px] px-[50px] ">
-             {filteredServices.map(service => (
+        
+      <div className={" flex items-center justify-center mx-auto my-auto md:flex-row flex-col md:gap-8 gap-3 py-[20px] " + styl}>
+            
+        {filteredServices.map(service => (
+                
                  <div key={service.id} className="bg-[white] padd border text-center shadow-[0_4px_8px_rgba(0,0,0,0.1)]  rounded-lg border-solid border-[#ddd] md:p-5 sm:p-4 p-2">
                    <Image 
 src="/service.jpg" 
@@ -50,10 +54,11 @@ priority
                      <h3 className="font-bold py-2">{service.title}</h3>
                      <p>{service.description}</p>
                  </div>
+                 
              ))}
-         </div>
-
         
+        </div>
+                 
            
         </section>
     );
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,10 +1,10 @@
 const Welcome = () => {
     return (
       <section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
-        {/* Overlay */}
+      
         <div className="absolute inset-0 bg-black opacity-20"></div>
   
-        {/* Text Content */}
+     
         <div className="relative text-center max-w-[1400px] mx-auto p-2">
           <h1 className="font-bold sm:text-[32px] text-[24px]">Welcome to Our Website</h1>
           <p className="sm:text-[24px] text-[20px]">Discover our amazing services and products.</p>
