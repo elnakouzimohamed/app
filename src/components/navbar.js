b08412fd0fdd4b9e32c580594d743763a1181cd0
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-21 14:05:04 +0200

    i

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
       
 
         <header>
-            <nav className="lg: flex justify-between items-center max-w-[1400px] mx-auto my-2 px-5 py-[15px] mb-2 sm:space-x-0 space-y-0">
+            <nav className="flex justify-between items-center max-w-[1400px] mx-auto my-2 px-5 py-[15px] mb-2 sm:space-x-0 space-y-0">
 
                 <div >
                      <Image 
@@ -21,9 +21,11 @@ export default function Navbar() {
                     />
                 </div>
                 <ul className="flex gap-5">
-                    <li><Link href="#home" className="no-underline text-[17px] text-[black] transition-all duration-[0.3s] ease-[ease-in-out] px-2.5 py-[5px] rounded-[5px] hover:bg-[rgba(0,0,0,0.1)] hover:shadow-[0px_4px_8px_rgba(0,0,0,0.2)] hover:rounded-[5px]">Home</Link></li>
-                    <li><Link href="#about" className="no-underline text-[17px] text-[black] transition-all duration-[0.3s] ease-[ease-in-out] px-2.5 py-[5px] rounded-[5px] hover:bg-[rgba(0,0,0,0.1)] hover:shadow-[0px_4px_8px_rgba(0,0,0,0.2)] hover:rounded-[5px]">About</Link></li>
-                    <li><Link href="#contact" className="no-underline text-[17px] text-[black] transition-all duration-[0.3s] ease-[ease-in-out] px-2.5 py-[5px] rounded-[5px] hover:bg-[rgba(0,0,0,0.1)] hover:shadow-[0px_4px_8px_rgba(0,0,0,0.2)] hover:rounded-[5px]">Contact</Link></li>
+                    <div className="no-underline text-[17px] text-[black] transition-all duration-[0.3s] ease-[ease-in-out] px-2.5 py-[5px] rounded-[5px] hover:bg-[rgba(0,0,0,0.1)] hover:shadow-[0px_4px_8px_rgba(0,0,0,0.2)] hover:rounded-[5px]">
+                    <li><Link href="#home">Home</Link></li>
+                    <li><Link href="#about" >About</Link></li>
+                    <li><Link href="#contact">Contact</Link></li>
+                    </div>
                 </ul>
                
             </nav>
