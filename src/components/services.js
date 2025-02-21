0ea4cb54f307204fef3e7b007f80e649758fef8b
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-21 15:42:00 +0200

    i

--- src/components/services.js
+++ src/components/services.js
@@ -12,9 +12,9 @@ const Services = () => {
     return (
         <section className=" gap-5 flex-wrap pt-2 pb-12">
             <h1 className="text-center font-bold  pt-[70px] pb-5 text-[32px]" >Check Out Our Services</h1>
-            <div className=" flex items-center justify-center mx-auto my-auto md:flex-row flex-col md:gap-8 gap-3  ">
+            <div className=" flex items-center justify-center mx-auto my-auto md:flex-row flex-col md:gap-8 gap-3 py-[20px] px-[80px] ">
                 {services.map(service => (
-                    <div key={service.id} className="bg-[white] border text-center shadow-[0_4px_8px_rgba(0,0,0,0.1)] p-5 rounded-lg border-solid border-[#ddd] md:w-[400px] sm:w-[350px] w-[300px]">
+                    <div key={service.id} className="bg-[white] padd border text-center shadow-[0_4px_8px_rgba(0,0,0,0.1)] p-4 rounded-lg border-solid border-[#ddd]">
                       <Image 
   src="/service.jpg" 
   alt={service.title} 
