75aa13d827b7d7cd616da2527bcb7c0b445a9916
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-21 15:44:02 +0200

    i

--- src/components/services.js
+++ src/components/services.js
@@ -14,7 +14,7 @@ const Services = () => {
             <h1 className="text-center font-bold  pt-[70px] pb-5 text-[32px]" >Check Out Our Services</h1>
             <div className=" flex items-center justify-center mx-auto my-auto md:flex-row flex-col md:gap-8 gap-3 py-[20px] px-[80px] ">
                 {services.map(service => (
-                    <div key={service.id} className="bg-[white] padd border text-center shadow-[0_4px_8px_rgba(0,0,0,0.1)] p-4 rounded-lg border-solid border-[#ddd]">
+                    <div key={service.id} className="bg-[white] padd border text-center shadow-[0_4px_8px_rgba(0,0,0,0.1)] p-5 rounded-lg border-solid border-[#ddd]">
                       <Image 
   src="/service.jpg" 
   alt={service.title} 
@@ -23,7 +23,7 @@ const Services = () => {
   style={{ width: "100%", height: "auto" }} 
   priority 
 />
-                        <h3 className="font-bold">{service.title}</h3>
+                        <h3 className="font-bold py-2">{service.title}</h3>
                         <p>{service.description}</p>
                     </div>
                 ))}
