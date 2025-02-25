5f128db729f159e85d1498063909d45c7ceb2028
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-25 17:27:50 +0200

    i

---
+++ src/components/banner.js
@@ -0,0 +1,111 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
+import Image from 'next/image';
+import tailwindConfig from "../../tailwind.config";
+
+import 'swiper/css';
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
+import "swiper/css/navigation"; 
+import "swiper/css/pagination"; 
+import { useRef } from 'react';
+import { Navigation, Pagination, Autoplay, Scrollbar, A11y } from "swiper/modules";
+
+export default function BannerSlider(){
+
+    
+
+            const prevRef = useRef(null);
+            const nextRef = useRef(null);
+  return (
+    <Swiper
+    modules={[Navigation, Pagination, Autoplay]}
+    spaceBetween={0}
+    slidesPerView={1}
+    navigation
+    pagination={{ clickable: true }}
+    autoplay={{ delay: 3000, disableOnInteraction: false }}
+    onInit={(swiper) => {
+        
+        swiper.params.navigation.prevEl = prevRef.current;
+        swiper.params.navigation.nextEl = nextRef.current;
+        swiper.navigation.init();
+        swiper.navigation.update();
+      }}
+    className="w-full"
+  >
+<button
+        ref={prevRef}
+        className="absolute top-1/2 left-5 transform -translate-y-1/2 bg-gray-800 text-white p-3 rounded-full hover:bg-gray-600 transition"
+      >
+        <FaChevronLeft size={24} />
+      </button>
+
+      <button
+        ref={nextRef}
+        className="absolute top-1/2 right-5 transform -translate-y-1/2 bg-gray-800 text-white p-3 rounded-full hover:bg-gray-600 transition"
+      >
+        <FaChevronRight size={24} />
+      </button>    
+    <SwiperSlide>
+      <section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
+   
+      <div className="absolute inset-0 bg-black opacity-20"></div>
+
+   
+      <div className="relative text-center max-w-[1400px] mx-auto p-2">
+      
+        <h1 className="font-bold sm:text-[32px] text-[24px]">Welcome to Our Website</h1>
+        
+        
+      
+        
+      </div>
+    </section>
+    </SwiperSlide>
+    <SwiperSlide>
+   
+
+    <section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
+   
+   <div className="absolute inset-0 bg-black opacity-20"></div>
+
+
+   <div className="relative text-center max-w-[1400px] mx-auto p-2">
+   
+   <p className="sm:text-[24px] text-[20px]">Discover our amazing services and products.</p>
+     
+     
+   
+     
+   </div>
+ </section>
+      
+     
+    </SwiperSlide>
+    <SwiperSlide> 
+<section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
+   
+   <div className="absolute inset-0 bg-black opacity-20"></div>
+
+
+   <div className="relative text-center max-w-[1400px] mx-auto p-2">
+   
+   <span className="sm:text-[24px] text-[20px]">
+  Join us and explore endless possibilities!
+</span>
+     
+     
+   
+     
+   </div>
+ </section>
+</SwiperSlide>
+
+    
+    
+     
+        </Swiper>  
+
+        
+  );
+};
--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -2,14 +2,15 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState } from 'react';
 import Image from 'next/image';
 import tailwindConfig from "../../tailwind.config";
-
+import BannerSlider from '@/components/banner';
 import 'swiper/css';
-
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
 import "swiper/css/navigation"; 
 import "swiper/css/pagination"; 
-
+import { useRef } from 'react';
 import { Navigation, Pagination, Autoplay, Scrollbar, A11y } from "swiper/modules";
-export default () => {
+
+export default function AboutUs(){
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -67,71 +68,11 @@ export default () => {
             const filteredFaqs = faqItems.filter(faq =>
               faq.question.toLowerCase().includes(searchTerm.toLowerCase())
             );
+
+          
   return (
-    <Swiper
-    modules={[Navigation, Pagination, Autoplay]}
-    spaceBetween={0}
-    slidesPerView={1}
-    navigation
-    pagination={{ clickable: true }}
-    autoplay={{ delay: 3000, disableOnInteraction: false }}
-    className="w-full"
-  >
-    
-    <SwiperSlide>
-      <section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
-   
-      <div className="absolute inset-0 bg-black opacity-20"></div>
-
-   
-      <div className="relative text-center max-w-[1400px] mx-auto p-2">
-      
-        <h1 className="font-bold sm:text-[32px] text-[24px]">Welcome to Our Website</h1>
-        
-        
-      
-        
-      </div>
-    </section>
-    </SwiperSlide>
-    <SwiperSlide>
-   
-
-    <section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
-   
-   <div className="absolute inset-0 bg-black opacity-20"></div>
-
-
-   <div className="relative text-center max-w-[1400px] mx-auto p-2">
-   
-   <p className="sm:text-[24px] text-[20px]">Discover our amazing services and products.</p>
-     
-     
-   
-     
-   </div>
- </section>
-      
-     
-    </SwiperSlide>
-    <SwiperSlide> 
-<section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
-   
-   <div className="absolute inset-0 bg-black opacity-20"></div>
-
-
-   <div className="relative text-center max-w-[1400px] mx-auto p-2">
-   
-   <span className="sm:text-[24px] text-[20px]">
-  Join us and explore endless possibilities!
-</span>
-     
-     
-   
-     
-   </div>
- </section>
-</SwiperSlide>
+    <Swiper>
+        <BannerSlider/>
         <section className="gap-5 flex-wrap pt-2 pb-12">
                     <h1 className="text-center font-bold pt-[70px] pb-5 text-[32px]">Check Out Our Services</h1>
                     
@@ -212,7 +153,9 @@ id="contact">
         </section>
     
     
-      
-    </Swiper>
+     
+        
+
+        </Swiper>      
   );
 };
