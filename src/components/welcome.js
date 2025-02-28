cae955d3a5a7e8a15d253b3b6cc56e4f8a3262e1
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-28 15:41:32 +0200

    i

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,4 +1,4 @@
-const Welcome = ({title="", description="" }) => {
+const Welcome = ({title, description }) => {
     return (
       
       <section className="relative w-full h-[500px] flex items-center justify-center text-white bg-cover bg-center sm:items-center " style={{ backgroundImage: "url('/background.jpeg')" }}>
