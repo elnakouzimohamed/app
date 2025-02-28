a3b8e3818fa13fd984ed7878f1a29bd73f4d6d0c
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-28 15:46:59 +0200

    i

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,7 @@
 import "@/styles/globals.css";
 import tailwindConfig from "../../tailwind.config";
-import Navbar from "@/components/navbar";
-import Welcome from "@/components/welcome";
-import Services from "@/components/services";
-import Contact from "@/components/contact";
-import Faq from "@/components/faq";
-import Footer from "@/components/footer";
-
 
 export default function App({ Component, pageProps }) {
   
-  return (
-    <>
-    <Navbar/>
-  <Welcome title="Welcome to our Website" description="Discover more about our product and services"></Welcome>
-  <Services></Services>
-
-<Contact/>
-
-<Faq/>
-
-
-
-  
- <Footer/>
- </>
-
-
-  )
+  return <Component {...pageProps} />;
 }
