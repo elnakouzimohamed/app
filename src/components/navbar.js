085c5ae3a3b4f9c5e5b93672bea1725f03c81b76
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-28 16:13:51 +0200

    i

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,14 +11,15 @@ export default function Navbar() {
             <nav className="flex justify-between items-center max-w-[1400px] mx-auto my-2 px-5 py-[15px] mb-2 sm:space-x-0 space-y-0">
 
                 <div >
+                    <Link href="/">
                      <Image 
-                      src="/logo2.jpeg"
+                      src="/logo.jpg"
                       alt="logo"
                       width={300} 
                       height={200} 
                       style={{ width: "100%", height: "auto" }} 
                        
-                    />
+                    /> </Link>
                 </div>
                 <ul className="flex gap-5">
                     <li><Link href="/" className="no-underline text-[17px] text-[black] transition-all duration-[0.3s] ease-[ease-in-out] px-2.5 py-[5px] rounded-[5px] hover:bg-[rgba(0,0,0,0.1)] hover:shadow-[0px_4px_8px_rgba(0,0,0,0.2)] hover:rounded-[5px]">Home</Link></li>
